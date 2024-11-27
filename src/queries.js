edd41afaf222a381b747a4bc33bd28d96f356518
achoi2k20 <achoi2k20@gmail.com>
2024-11-26 22:40:52 -0800

    edited queries folder

--- src/queries.js
+++ src/queries.js
@@ -2,24 +2,24 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.queries = void 0;
 exports.queries = {
-    viewDepartments: 'SELECT * FROM department',
+    viewDepartments: 'SELECT * FROM departments',
     viewRoles: `
-        SELECT role.id, role.title, role.salary, department.name AS department
-        FROM role
-        INNER JOIN department ON role.department_id = department.id
+        SELECT roles.id, roles.title, roles.salary, departments.name AS departments
+        FROM roles
+        INNER JOIN departments ON roles.department_id = departments.id
     `,
     viewEmployees: `
         SELECT 
             e.id, e.first_name, e.last_name, 
-            role.title, department.name AS department, 
-            role.salary, m.first_name AS manager
-        FROM employee e
-        INNER JOIN role ON e.role_id = role.id
-        INNER JOIN department ON role.department_id = department.id
-        LEFT JOIN employee m ON e.manager_id = m.id
+            roles.title, departments.name AS departments, 
+            roles.salary, m.first_name AS manager
+        FROM employees e
+        INNER JOIN roles ON e.role_id = roles.id
+        INNER JOIN departments ON roles.department_id = departments.id
+        LEFT JOIN employees m ON e.manager_id = m.id
     `,
-    addDepartment: 'INSERT INTO department (name) VALUES ($1)',
-    addRole: 'INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)',
-    addEmployee: 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)',
-    updateEmployeeRole: 'UPDATE employee SET role_id = $1 WHERE id = $2',
+    addDepartment: 'INSERT INTO departments (name) VALUES ($1)',
+    addRole: 'INSERT INTO roles (title, salary, department_id) VALUES ($1, $2, $3)',
+    addEmployee: 'INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)',
+    updateEmployeeRole: 'UPDATE employees SET role_id = $1 WHERE id = $2',
 };
